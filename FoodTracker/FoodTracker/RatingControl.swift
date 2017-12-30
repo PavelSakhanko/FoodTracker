3f8ac0408b65af774e99b7790a6cfe50ce8a2647
Pavel Sakhanko <pavelsakhanko@gmail.com>
2017-12-30 09:50:27 +0300

    Created RatingControl class and overrided his initializers.

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,20 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Pavel Sakhanko on 30.12.2017.
+//  Copyright © 2017 Pavel Sakhanko. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+//MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
